8f19c863aa90c04752a11670a1e17537a91b44bd
HDamavandi <42955679+HDamavandi@users.noreply.github.com>
2023-12-12 10:24:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,37 @@ user_input = st.text_input("Please enter the consumer's failure description:")
 analyzer = SentimentIntensityAnalyzer()
 
 def Comp(user_input):
-    Neg = analyzer.polarity_scores(user_input).get('compound')
-    return Neg
+    
 
 
 Gender = st.selectbox('Gender', ["Female", "Male","Unknown", "Unspecified"])
 Relation = st.selectbox('Primary victim', ["Yes", "No"])
 
+def Resp(Text, Gender, Relation):
+    if Gender == "Female":
+        GenderCoeff = 1
+    elif Gender == "Male":
+        GenderCoeff = .0652719
+    elif Gender == "Unknown":
+        GenderCoeff = .277797
+    else:
+        GenderCoeff = .2617218
+    if Relation == "Yes":
+        RelationCoeff =  -.0689343
+    else:
+        RelationCoeff = 1
+        
+    Neg = analyzer.polarity_scores(Text).get('compound')
 
-st.write("The likelihood that this consumer avoids your brand in the future is:", Comp(user_input))
+    Response = -.19 * Neg + GenderCoeff + RelationCoeff   
+    
+    return Response
+    
+    
+
+Response = Resp(user_input, Gender, Relation)
+
+st.write("The likelihood that this consumer avoids your brand in the future is:", Response)
 
 
 
