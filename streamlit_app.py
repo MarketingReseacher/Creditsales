e6d24774d1e0e4d20a327bb7077de871b5ac8f01
HDamavandi <42955679+HDamavandi@users.noreply.github.com>
2023-12-12 09:13:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,19 @@
-import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
 st.title("Brand Avoidance Likelihood")
 
-user_input = st.text_input("Enter failure description")
+user_input = st.text_area("Enter failure description")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+analyzer = SentimentIntensityAnalyzer()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+def Comp(user_input):
+    Neg = analyzer.polarity_scores(user_input).get('neg')
+    return Neg
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+st.text(Comp(user_input))
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
