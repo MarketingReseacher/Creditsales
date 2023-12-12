a99761589800118d6ede7b8d77b0b89e164b3d1d
HDamavandi <42955679+HDamavandi@users.noreply.github.com>
2023-12-12 11:25:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,35 +16,34 @@ st.write("The consumer is the primary victim:", Relation)
 user_input = st.text_input("Please enter the consumer's failure description:")
 
 
-def Resp(T, G, R):
-    
-    if T == "":
+def Resp(T, G, R): 
+    if G == "Female":
+        GenderCoeff = 0
+    elif G == "Male":
+        GenderCoeff = .01
+    elif G == "Unknown":
+        GenderCoeff = .20
+    else:
+        GenderCoeff = .16
+            
+    if R == "Yes":
+        RelationCoeff =  -.06
+    else:
+        RelationCoeff = 0
+
+    st.write("The Relationship coeff is", RelationCoeff)
+
+    analyzer = SentimentIntensityAnalyzer()
+    Neg = analyzer.polarity_scores(T).get('compound') * 100
+
+    Cop = .03
+
+    Odds = ((-.21 * Neg) + GenderCoeff + RelationCoeff + (1.05 * Cop) - 1.48)
+    Prob = (Odds/(1+Odds)) 
+
+    if T = "":
         Response = 0
     else:
-        
-        if G == "Female":
-            GenderCoeff = 0
-        elif G == "Male":
-            GenderCoeff = .01
-        elif G == "Unknown":
-            GenderCoeff = .20
-        else:
-            GenderCoeff = .16
-            
-        if R == "Yes":
-            RelationCoeff =  -.06
-        else:
-            RelationCoeff = 0
-
-        st.write("The Relationship coeff is", RelationCoeff)
-
-        analyzer = SentimentIntensityAnalyzer()
-        Neg = analyzer.polarity_scores(T).get('compound') * 100
-
-        Cop = .03
-
-        Odds = ((-.21 * Neg) + GenderCoeff + RelationCoeff + (1.05 * Cop) - 1.48)
-        Prob = (Odds/(1+Odds)) 
         Response = round(Prob, 2) * 100
         
     return Response
