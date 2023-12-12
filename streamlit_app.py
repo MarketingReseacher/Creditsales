296a1a46b907d59ef9cfc00227211eeba0104c51
HDamavandi <42955679+HDamavandi@users.noreply.github.com>
2023-12-12 09:16:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
+import nltk
 
 st.title("Brand Avoidance Likelihood")
 
