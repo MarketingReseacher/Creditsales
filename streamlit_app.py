aee2520ff6101113e51bd4a81d156b7dcd794e5d
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2023-12-16 12:21:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,9 @@ PastDic = ['past', 'previously', 'previous', 'earlier', 'historically', 'formerl
 
 
 FutureDic = ['future', 'eventually', 'prospectively', 'henceforth', 'everytime', "everyday", "anytime", 'tomorrow', 'imminently',  'hereafter', 'hereon', 
-             'henceforward', 'longrun', 'longterm', 'forthcoming', 'upcoming', 'oncoming', 'incoming', 'impending', foreseeable', 'will', 'shall', 'wont', 'might',  'may', 
-             'aftertime', 'thereafter', 'potential', 'potentially', 'intermittently', 'successively', 'supposedly']
+             'henceforward', 'longrun', 'longterm', 'forthcoming', 'upcoming', 'oncoming', 'incoming', 'impending', foreseeable',
+              'will', 'shall', 'wont', 'might',  'may', 
+              'aftertime', 'thereafter', 'potential', 'potentially', 'intermittently', 'successively', 'supposedly']
 
 analyzer = SentimentIntensityAnalyzer()
 
