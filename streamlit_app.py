7bc0fafae2c5147743a17114b855c05498aa6112
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-03-23 20:05:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,7 +143,7 @@ else:
         return Model
         
     Model = RF()
-    BR = Model.predict(Xnew)[0] + 7
+    BR = Model.predict(Xnew)[0] + 6
     Response = round(BR, 2)
     st.write("#### Bankruptcy risk:", Response)
 
